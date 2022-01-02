efec97a9cf4f6d2fa35ab176f4da8478790ccf7c
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-02 23:36:21 +0300

    add static files dirs

--- producthuntturkey/producthuntturkey/settings.py
+++ producthuntturkey/producthuntturkey/settings.py
@@ -118,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
