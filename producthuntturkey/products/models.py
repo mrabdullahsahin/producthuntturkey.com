478bf1ddb026d8c062eb1b7d94fc07bf8a9b52ee
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-08 20:40:42 +0300

    show team size value on admin panel

--- producthuntturkey/products/models.py
+++ producthuntturkey/products/models.py
@@ -11,6 +11,9 @@ class TeamSize(models.Model):
     size = models.CharField(max_length=15, null=True)
     slug = models.SlugField(max_length=15, unique=True, null=True)
 
+    def __str__(self):
+        return self.size
+
 class Product(models.Model):
     product_name = models.CharField(max_length=200)
     product_description = models.TextField(blank=True, null=True)
