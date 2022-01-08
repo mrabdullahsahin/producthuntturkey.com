e925d3aada408c7f1eb39cdd1c0225e14a68312f
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-08 14:42:16 +0300

    create TeamSize model

--- producthuntturkey/products/models.py
+++ producthuntturkey/products/models.py
@@ -3,3 +3,7 @@ from django.db import models
 class City(models.Model):
     city = models.CharField(max_length=50, null=True)
     slug = models.SlugField(max_length=50, unique=True, null=True)
+
+class TeamSize(models.Model):
+    size = models.CharField(max_length=15, null=True)
+    slug = models.SlugField(max_length=15, unique=True, null=True)
