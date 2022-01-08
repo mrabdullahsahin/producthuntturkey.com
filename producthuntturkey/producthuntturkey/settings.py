b04487cfaaf1ee02fa8720effcb9d52aecaf9e22
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-08 21:45:23 +0300

    add MEDIA_URL and MEDIA_ROOT on settings

--- producthuntturkey/producthuntturkey/settings.py
+++ producthuntturkey/producthuntturkey/settings.py
@@ -124,3 +124,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# MEDIA FILES
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
