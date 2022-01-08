169fe41a037c909a4cf1eb2032d80c842032030c
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-08 20:23:34 +0300

    show city name on admin panel

--- producthuntturkey/products/models.py
+++ producthuntturkey/products/models.py
@@ -4,6 +4,9 @@ class City(models.Model):
     city = models.CharField(max_length=50, null=True)
     slug = models.SlugField(max_length=50, unique=True, null=True)
 
+    def __str__(self):
+        return self.city
+
 class TeamSize(models.Model):
     size = models.CharField(max_length=15, null=True)
     slug = models.SlugField(max_length=15, unique=True, null=True)
