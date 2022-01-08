851b2927c73e5ec99336f523ea328f03c7d3148d
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-08 20:15:32 +0300

    add TeamSize model on admin panel

--- producthuntturkey/products/admin.py
+++ producthuntturkey/products/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from . models import City
+from . models import City, TeamSize
 
 @admin.register(City)
 class CityAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('city',)}
+
+@admin.register(TeamSize)
+class TeamSize(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('size',)}
