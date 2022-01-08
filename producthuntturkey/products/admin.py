ac8e55d7e84ac766ffb464257284b5d5a063e3d6
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-08 20:11:30 +0300

    add City model on admin panel

--- producthuntturkey/products/admin.py
+++ producthuntturkey/products/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import City
 
-# Register your models here.
+@admin.register(City)
+class CityAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('city',)}
