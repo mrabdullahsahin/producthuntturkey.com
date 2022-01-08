f17681a065ba588ee9db023c7600bba6dee8370c
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-08 14:32:29 +0300

    create City model

--- producthuntturkey/products/models.py
+++ producthuntturkey/products/models.py
@@ -1,3 +1,5 @@
 from django.db import models
 
-# Create your models here.
+class City(models.Model):
+    city = models.CharField(max_length=50, null=True)
+    slug = models.SlugField(max_length=50, unique=True, null=True)
