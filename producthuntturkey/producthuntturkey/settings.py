5dc8d7f01eff8cc40cf8125dfa78ee535044232d
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-09 16:31:21 +0300

    add openstartup to INSTALLED_APPS

--- producthuntturkey/producthuntturkey/settings.py
+++ producthuntturkey/producthuntturkey/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
+    'openstartup.apps.OpenstartupConfig',
 ]
 
 MIDDLEWARE = [
