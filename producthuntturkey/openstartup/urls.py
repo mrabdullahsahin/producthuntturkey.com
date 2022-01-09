da1e91506064bc7f1acbdaa0291e16e2afc32785
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-09 16:40:54 +0300

    fix open startup page url

--- producthuntturkey/openstartup/urls.py
+++ producthuntturkey/openstartup/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.openpage, name='open-startup'),
+    path('open-startup', views.openpage, name='open-startup'),
 ]
