727e4cd258d1be0f4aacc1dba7ae1b63a0c24a46
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-09 11:49:44 +0300

    send product data view to index.html

--- producthuntturkey/products/views.py
+++ producthuntturkey/products/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from . models import Product
 
 def index(request):
-    return render(request, 'index.html')
+    products = Product.objects.all().order_by('-product_launch_date')
+
+    context = {
+        'products': products
+    }
+
+    return render(request, 'index.html', context)
