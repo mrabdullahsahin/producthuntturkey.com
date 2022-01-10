2819e2accaa72f02cf695e052bbf52b496027b78
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-10 20:00:36 +0300

    add new app on root urls file

--- producthuntturkey/producthuntturkey/urls.py
+++ producthuntturkey/producthuntturkey/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('products.urls')),
     path('', include('openstartup.urls')),
+    path('', include('addyourstartup.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
