fb3b0e3c1846217009fcacab33ca0083604a374d
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-10 19:59:36 +0300

    add new app on INSTALLED_APPS called addyourstartup

--- producthuntturkey/producthuntturkey/settings.py
+++ producthuntturkey/producthuntturkey/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'products.apps.ProductsConfig',
     'openstartup.apps.OpenstartupConfig',
+    'addyourstartup.apps.AddyourstartupConfig',
 ]
 
 MIDDLEWARE = [
