5c443ad4c256f288315e24fb3abfba93cbe9dd43
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-22 17:28:01 +0300

    update addyourstartup func for form

--- producthuntturkey/addyourstartup/views.py
+++ producthuntturkey/addyourstartup/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.http import HttpResponse
+
+from .forms import AddYourStartupForm
 
 def addyourstartup(request):
-    return render(request, 'add-your-startup.html')
+    #return render(request, 'add-your-startup.html')
+    if request.method == "POST":
+        form = AddYourStartupForm(request.POST)
+        form.save()
+        messages.success(request, "Your product was successfully submitted!")
+        return redirect('add-your-startup')
+
+    context = {
+        'form': form
+    }
+
+    return render(request, 'add-your-startup.html', context)
