e4cd4fac74ed7b7b0366dc17ab9ec1d5c94dd0e2
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-01-22 15:49:49 +0300

    add AddYourStartupArea model on admin panel

--- producthuntturkey/addyourstartup/admin.py
+++ producthuntturkey/addyourstartup/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . models import AddYourStartupArea
 
-# Register your models here.
+@admin.register(AddYourStartupArea)
+class AddYourStartupAreaAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('product_name',)}
