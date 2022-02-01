767bf4d8147c53553031fa73b6f0fc798dc98613
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-02-01 22:02:43 +0300

    add languages for making a multi language

--- producthuntturkey/producthuntturkey/settings.py
+++ producthuntturkey/producthuntturkey/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 import os
 from pathlib import Path
+from django.utils.translation import gettext_lazy as _
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -115,6 +116,10 @@ USE_I18N = True
 
 USE_TZ = True
 
+LANGUAGES = (
+    ('en', _('English')),
+    ('tr', _('Turkish')),
+)
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
