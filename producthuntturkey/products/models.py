45d22019f194dda0505d576c499168a2aa07db31
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-02-01 22:41:03 +0300

    update products model for multi language process

--- producthuntturkey/products/models.py
+++ producthuntturkey/products/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 class City(models.Model):
     city = models.CharField(max_length=50, null=True)
@@ -16,7 +17,7 @@ class TeamSize(models.Model):
 
 class Product(models.Model):
     product_name = models.CharField(max_length=200)
-    product_description = models.TextField(blank=True, null=True)
+    product_description = models.TextField(_('product_description'), blank=True, null=True)
     slug = models.SlugField(max_length=200, unique=True, null=True)
     product_picture = models.ImageField(upload_to="products/%Y/%m/%d/", default="default/producthuntturkey-logo.png")
     product_city = models.ForeignKey(City, blank=True, null=True, on_delete=models.DO_NOTHING)
@@ -25,7 +26,7 @@ class Product(models.Model):
     product_team_size = models.ForeignKey(TeamSize, blank=True, null=True, on_delete=models.DO_NOTHING)
     product_twitter = models.CharField(max_length=250, blank=True, null=True)
     product_owner_twitter = models.CharField(max_length=250, blank=True, null=True)
-    product_launch_date = models.DateField()
+    product_launch_date = models.DateField(_('product_launch_date'))
     product_hunt_votes = models.CharField(max_length=6, default=0, blank=True, null=True)
     is_published_telegram = models.BooleanField(default=False)
     is_published_twitter = models.BooleanField(default=False)
