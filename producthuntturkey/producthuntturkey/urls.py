33db5939b7189ff6583cbba07202e34d9ee569f0
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-02-01 23:14:30 +0300

    add localization rules on urls

--- producthuntturkey/producthuntturkey/urls.py
+++ producthuntturkey/producthuntturkey/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf.urls.i18n import i18n_patterns
+from django.utils.translation import gettext_lazy as _
 
-urlpatterns = [
+urlpatterns = i18n_patterns(
     path('admin/', admin.site.urls),
     path('rosetta/', include('rosetta.urls')),
     path('', include('products.urls')),
     path('', include('openstartup.urls')),
     path('', include('addyourstartup.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
