7623eb80716caf63a42b5cb39cf5e63ae41e6df8
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-02-14 22:39:29 +0300

    create add_to_product_table function for admin file in addyourstartup app

--- producthuntturkey/addyourstartup/admin.py
+++ producthuntturkey/addyourstartup/admin.py
@@ -1,6 +1,31 @@
 from django.contrib import admin
 from . models import AddYourStartupArea
+from products.models import Product
+
+@admin.action(description='Product Tablosuna Ekle')
+def add_to_product_table(self, request, queryset):
+    for startup in queryset:
+        product = Product()
+        product.product_name = startup.product_name
+        product.slug = startup.slug
+        product.product_twitter = startup.product_twitter
+        product.product_owner_twitter = startup.product_owner_twitter
+        product.product_ph_link = startup.product_ph_link
+        product.product_website = startup.product_website
+        product.product_launch_date = startup.product_launch_date
+        product.product_team_size = startup.product_team_size
+        product.product_picture = startup.product_picture
+        product.product_city = startup.product_city
+        product.set_current_language('en')
+        product.product_description = startup.product_about_en
+        product.set_current_language('tr')
+        product.product_description = startup.product_about_tr
+        product.save()
+        self.message_user(
+            request,"Product tablosuna kayıt başarıyla gerçekleştirildi.",
+        )
 
 @admin.register(AddYourStartupArea)
 class AddYourStartupAreaAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('product_name',)}
+    actions = [add_to_product_table]
