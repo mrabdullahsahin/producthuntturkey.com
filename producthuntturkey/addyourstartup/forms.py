66ff36b5861ff84cee1d77569bd97b9b9cfe87c9
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-02-15 21:35:58 +0300

    customize forms presentation

--- producthuntturkey/addyourstartup/forms.py
+++ producthuntturkey/addyourstartup/forms.py
@@ -1,12 +1,20 @@
 from django.forms import ModelForm
 from django import forms
 from .models import AddYourStartupArea
+from products.models import City, TeamSize
 
 class AddYourStartupForm(ModelForm):
-
-    product_launch_date = forms.DateField(widget=forms.DateInput(attrs={
-        'type': 'date'
-    }))
+    product_name = forms.CharField(max_length = 250, required=True)
+    product_about_en = forms.CharField(required=True,widget=forms.widgets.Textarea())
+    product_about_tr = forms.CharField(required=True,widget=forms.widgets.Textarea())
+    product_twitter = forms.CharField(max_length = 250, required=False)
+    product_owner_twitter = forms.CharField(max_length = 250, required=False)
+    product_ph_link = forms.URLField(max_length = 250, required=True)
+    product_website = forms.URLField(max_length = 250, required=True)
+    product_team_size = forms.ModelChoiceField(queryset=TeamSize.objects.all(), required=True)
+    product_picture = forms.ImageField(required=True, widget=forms.FileInput(attrs={'accept': '.gif, .jpg, .jpeg, .png'}))
+    product_city = forms.ModelChoiceField(queryset=City.objects.all(), required=True)
+    product_launch_date = forms.DateField(required=True, widget=forms.DateInput(attrs={'type': 'date'}))
 
     class Meta:
         model = AddYourStartupArea
