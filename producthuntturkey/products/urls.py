398e46cfa89124fc42e106eb6db8f05a6be541ec
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-02-19 14:23:25 +0300

    make url for product listing by city

--- producthuntturkey/products/urls.py
+++ producthuntturkey/products/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('product/<slug:product_slug>', views.index, name='product_detail'),
+    path('city/<slug:city_slug>', views.index, name='city-list'),
 ]
