7b79aa5dc2b975e0e7a7bed7cfe4788561aea1b3
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-02-19 15:26:12 +0300

    make product listing by team size

--- producthuntturkey/products/views.py
+++ producthuntturkey/products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, get_object_or_404
-from . models import Product, City
+from . models import Product, City, TeamSize
 
-def index(request, product_slug=None, city_slug=None):
+def index(request, product_slug=None, city_slug=None, teamsize_slug=None):
     if product_slug != None:
         product = Product.objects.get(slug = product_slug)
 
@@ -20,6 +20,16 @@ def index(request, product_slug=None, city_slug=None):
         }
 
         return render(request, 'city.html', context)
+
+    elif teamsize_slug != None:
+        team_page = get_object_or_404(TeamSize, slug=teamsize_slug)
+        products = Product.objects.all().filter(is_avaliable=True, product_team_size = team_page).order_by('-product_launch_date')
+
+        context = {
+            'products': products
+        }
+
+        return render(request, 'team-size.html', context)
 
     else:
         products = Product.objects.filter(is_avaliable=True).order_by('-product_launch_date')
