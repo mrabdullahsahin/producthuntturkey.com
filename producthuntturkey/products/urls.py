36781be43cfd0fb919d24298c02d5b64348a43a7
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-02-19 15:27:01 +0300

    make url for product listing by team size

--- producthuntturkey/products/urls.py
+++ producthuntturkey/products/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('product/<slug:product_slug>', views.index, name='product_detail'),
     path('city/<slug:city_slug>', views.index, name='city-list'),
+    path('team-size/<slug:teamsize_slug>', views.index, name='team-size-list'),
 ]
