e0de208663d0d9dd2bf2f96fef3b4bb69a7a30ba
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-02-19 14:22:37 +0300

    make product listing by city

--- producthuntturkey/products/views.py
+++ producthuntturkey/products/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
-from . models import Product
+from django.shortcuts import render, get_object_or_404
+from . models import Product, City
 
-def index(request, product_slug=None):
+def index(request, product_slug=None, city_slug=None):
     if product_slug != None:
         product = Product.objects.get(slug = product_slug)
 
@@ -10,6 +10,16 @@ def index(request, product_slug=None):
         }
 
         return render(request, 'product-detail.html', context)
+
+    elif city_slug != None:
+        city_page = get_object_or_404(City, slug=city_slug)
+        products = Product.objects.all().filter(is_avaliable=True, product_city = city_page).order_by('-product_launch_date')
+
+        context = {
+            'products': products
+        }
+
+        return render(request, 'city.html', context)
 
     else:
         products = Product.objects.filter(is_avaliable=True).order_by('-product_launch_date')
