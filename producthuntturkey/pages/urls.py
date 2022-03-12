0547af877ff2bf626035e603230e05a40ff87552
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-03-12 17:16:17 +0300

    create ambassadors url

--- producthuntturkey/pages/urls.py
+++ producthuntturkey/pages/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('privacy-policy', views.privacy_policy, name='privacy-policy'),
     path('terms-of-service', views.terms_of_service, name='terms-of-service'),
+    path('ambassadors', views.ambassadors, name='ambassadors'),
 ]
