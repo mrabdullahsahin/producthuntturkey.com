bce3a6dd85ecfcec87f6b996ea06900fe1953a6a
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-03-12 16:56:46 +0300

    create terms of service url in pages

--- producthuntturkey/pages/urls.py
+++ producthuntturkey/pages/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('privacy-policy', views.privacy_policy, name='privacy-policy'),
+    path('terms-of-service', views.terms_of_service, name='terms-of-service'),
 ]
