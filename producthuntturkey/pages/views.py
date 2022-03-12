d2cc8bd690ff2ee18d2340b89aaf6aad58ffa9ea
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-03-12 16:55:42 +0300

    create terms of service view in pages

--- producthuntturkey/pages/views.py
+++ producthuntturkey/pages/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def privacy_policy(request):
     return render(request, 'privacy-policy.html')
+
+def terms_of_service(request):
+    return render(request, 'terms-of-service.html')
