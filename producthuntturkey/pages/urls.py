2e41e0760d7c8ec1b8c8f70fdf96bfd012b5e2a6
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-03-14 20:44:23 +0300

    create url for what is product hunt

--- producthuntturkey/pages/urls.py
+++ producthuntturkey/pages/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('privacy-policy', views.privacy_policy, name='privacy-policy'),
     path('terms-of-service', views.terms_of_service, name='terms-of-service'),
     path('ambassadors', views.ambassadors, name='ambassadors'),
+    path('what-is-product-hunt', views.what_is, name='what-is'),
 ]
