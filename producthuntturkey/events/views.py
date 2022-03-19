983fe484b4834cc9d0dc734fe1b0f8f7bd869a79
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-03-19 19:44:22 +0300

    write city slug situation for show event function

--- producthuntturkey/events/views.py
+++ producthuntturkey/events/views.py
@@ -14,7 +14,26 @@ def show_event(request, event_slug=None, event_city_slug=None):
         return render(request, 'event-detail.html', context)
 
     elif event_city_slug != None:
-        pass
+        city_page = get_object_or_404(City, slug=event_city_slug)
+        city = City.objects.get(slug=event_city_slug)
+        events_list = Event.objects.all().filter(is_avaliable=True, event_city = city_page).order_by('-event_date').all()
+
+        page = request.GET.get('page', 1)
+
+        paginator = Paginator(events_list, 3)
+        try:
+            events = paginator.page(page)
+        except PageNotAnInteger:
+            events = paginator.page(1)
+        except EmptyPage:
+            events = paginator.page(paginator.num_pages)         
+
+        context = {
+            'events': events,
+            'city': city
+        }
+
+        return render(request, 'event-city.html', context)
 
     else:
         events_list = Event.objects.filter(is_avaliable=True).order_by('-event_date').all()
