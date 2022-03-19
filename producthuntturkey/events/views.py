171eb6e070537bf4545f86d2c9ce64d80c3e92da
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-03-19 18:00:47 +0300

    create show event function

--- producthuntturkey/events/views.py
+++ producthuntturkey/events/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from . models import Event
+from products.models import City
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
-# Create your views here.
+def show_event(request, event_slug=None, event_city_slug=None):
+    if product_slug != None:
+        pass
+
+    elif event_city_slug != None:
+        pass
+
+    else:
+        events_list = Event.objects.filter(is_avaliable=True).order_by('-event_date').all()
+        page = request.GET.get('page', 1)
+
+        paginator = Paginator(events_list, 3)
+        try:
+            events = paginator.page(page)
+        except PageNotAnInteger:
+            events = paginator.page(1)
+        except EmptyPage:
+            events = paginator.page(paginator.num_pages)
+
+        context = {
+            'events': events
+        }
+
+        return render(request, 'events.html', context)
