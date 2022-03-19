31d672298cda3618a890ed1e2f4293dc440875c4
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-03-19 19:46:34 +0300

    write event slug and event city slug url for events

--- producthuntturkey/events/urls.py
+++ producthuntturkey/events/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     path('events', views.show_event, name='show-event'),
+    path('event/<slug:event_slug>', views.show_event, name='event-detail'),
+    path('events/city/<slug:event_city_slug>', views.show_event, name='events-city-list'),
 ]
