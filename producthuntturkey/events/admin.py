13c4ca6ff4fd83585053981c313464b7190461bf
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-03-19 17:47:14 +0300

    show events on admin panel

--- producthuntturkey/events/admin.py
+++ producthuntturkey/events/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from parler.admin import TranslatableAdmin
+from . models import Event
 
-# Register your models here.
+class EventAdmin(TranslatableAdmin):
+    list_display = ('event_name','event_city','is_avaliable')
+    list_filter = ('is_avaliable','event_city')
+    search_fields = ('event_name','event_city')
+    prepopulated_fields = {'slug': ('event_name',)}
+
+admin.site.register(Event, EventAdmin)
