78f4ff2456b61c30eaf755361fb8138a6aa331d6
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-03-21 22:50:51 +0300

    change settings file location and update BASE_DIR

--- producthuntturkey/producthuntturkey/settings/settings.py
+++ producthuntturkey/producthuntturkey/settings/settings.py
@@ -19,7 +19,7 @@ env = environ.Env()
 environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Quick-start development settings - unsuitable for production
