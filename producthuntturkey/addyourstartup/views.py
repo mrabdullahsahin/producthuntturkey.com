1e8c712654262b2bd9abaa7fdceda9ae7b835922
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-06-12 07:45:10 +0300

    change telegram api name on addyourstartup app

--- producthuntturkey/addyourstartup/views.py
+++ producthuntturkey/addyourstartup/views.py
@@ -18,10 +18,10 @@ def addyourstartup(request):
 
             product_info = "Ürünün Adı: {}\n Ürünün Product Hunt Linki: {}\n Ürünün Websitesinin Linki: {}\n Ürünün Product Hunt Yayınlanma Tarihi: {}\n Ürünün Twitter Kullanıcı Adı: {}\n Ürün Geliştiricisinin Twitter Kullanıcı Adı: {}\n Ürünün Takım Büyüklüğü: {}\n Ürünün Geliştirildiği Şehir: {}\n Ürünün Türkçe Açıklaması: {}\n Ürünün İngilizce Açıklaması: {}\n".format(form.instance.product_name, form.instance.product_ph_link, form.instance.product_website, form.instance.product_launch_date, form.instance.product_twitter, form.instance.product_owner_twitter, form.instance.product_team_size, form.instance.product_city, form.instance.product_about_tr, form.instance.product_about_en)
 
-            url = 'https://api.telegram.org/bot'+ env("TELEGRAM_BOT_API") +'/sendMessage'
+            url = 'https://api.telegram.org/bot'+ env("TELEGRAM_ADMIN_BOT_API") +'/sendMessage'
 
             data = {
-                'chat_id': env("TELEGRAM_CHAT_ID"), 
+                'chat_id': env("TELEGRAM_ADMIN_CHAT_ID"), 
                 'text': product_info
             }
         
