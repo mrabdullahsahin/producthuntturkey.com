9545d49718b6e2e772fd0d7b3b8d05a4cc296c8b
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-06-28 18:37:50 +0300

    delete parler import

--- producthuntturkey/launchcalendar/models.py
+++ producthuntturkey/launchcalendar/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from parler.models import TranslatableModel, TranslatedFields
 from products.models import City, TeamSize
 from django.utils.text import slugify
 from datetime import datetime
