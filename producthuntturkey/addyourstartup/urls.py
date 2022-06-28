94dabbd89852b724f30c9ee7a3dcc27e1c8f2e22
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-06-28 20:52:33 +0300

    create add your future startup page, urls and view

--- producthuntturkey/addyourstartup/urls.py
+++ producthuntturkey/addyourstartup/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('add-your-startup', views.addyourstartup, name='add-your-startup'),
+    path('add-your-future-startup', views.addyourfuturestartup, name="add-your-future-startup")
 ]
