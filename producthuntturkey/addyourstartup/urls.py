dbe793231e4c724972663be639cd2934920076b5
Abdullah Sahin <mrabdullahsahin@users.noreply.github.com>
2022-06-28 21:00:29 +0300

    delete add your future startup

--- producthuntturkey/addyourstartup/urls.py
+++ producthuntturkey/addyourstartup/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('add-your-startup', views.addyourstartup, name='add-your-startup'),
-    path('add-your-future-startup', views.addyourfuturestartup, name="add-your-future-startup")
+    path('add-your-startup', views.addyourstartup, name='add-your-startup')
 ]
